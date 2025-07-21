454f6608f297a4f33578442d2b4817481ddacd3a
Mohit-Saini-22 <mohit2356saini@gmail.com>
2025-07-21 13:48:27 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {
+       height: {
+        '128': '85vh',
+      }
+    },
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+         times: ['"Times New Roman"', 'Times', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
